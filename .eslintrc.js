4234ee7844c9e38276ef57bfba88ca66602fe6f9
Quantic Potatoes <jdieuze@gmail.com>
2019-06-30 16:41:37 +0200

    :tada: first commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential', 'airbnb-base'],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e' // for e.returnvalue
+      ]
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      optionalDependencies: ['test/unit/index.js']
+    }],
+    // Temporary fix for failed resolving in eslint-plugin-import
+    // We need to use parcel or jest resolver.
+    // Tried with existing jest resolver but no luck.
+    // https://www.npmjs.com/package/eslint-import-resolver-jest
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
